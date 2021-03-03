a8370d40ddd594219bbb3db3c84ee3cfc614cec3
Tantely Rabefarihy <tantelyrab@gmail.com>
2021-03-02 19:01:26 -0500

    packages updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `My Portfolio`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-theme-ui`,
-      options: {
-        preset: `gatsby-theme-ui-preset`,
-      },
-    },
-
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -20,6 +13,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
