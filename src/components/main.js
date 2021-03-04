27f7a92a6e4b965774a0fa76bd098fab837f70bd
Tantely Rabefarihy <tantelyrab@gmail.com>
2021-03-04 18:30:30 -0500

    create main component

---
+++ src/components/main.js
@@ -0,0 +1,46 @@
+import React from "react"
+import "../styles/styles.css"
+
+const Main = () => {
+  return (
+    <>
+      <main>
+        <section id="welcome-section">
+          <h1>Hey there 👋🏼 I'm Tantely.</h1>
+          <p>Web Developer</p>
+        </section>
+        <section id="projects">
+          <h2>These are some of my projects</h2>
+          <div>
+            <div class="project-tile">
+              <a
+                href="https://github.com/tantely-rabefarihy/covid19-app-tracker"
+                target="_blank"
+              >
+                <img
+                  src="https://github.com/tantely-rabefarihy/covid19-app-tracker/raw/main/client/screenshots/Covid-app.png"
+                  alt="project"
+                />
+              </a>
+              <p class="description">Covid tracker app</p>
+            </div>
+            <div class="project-tile"></div>
+          </div>
+        </section>
+        <section id="collaboration">
+          <h2>Let's collaborate !</h2>
+
+          <a
+            id="profile-link"
+            href="https://github.com/tantely-rabefarihy"
+            target="_blank"
+          >
+            <p>You can see my Github profile</p>
+          </a>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default Main
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
-// import { Link } from "gatsby"
 // import SEO from "../components/seo"
 import Layout from "../components/layout"
 import Helmet from "react-helmet"
 import Header from "../components/header"
 import GlobalStyle from "../styles/globalstyles"
-import "../styles/styles.css"
+import Main from "../components/main"
 
 const IndexPage = () => (
   <React.Fragment>
@@ -22,41 +21,7 @@ const IndexPage = () => (
     <GlobalStyle />
     <Layout>
       <Header />
-      <main>
-        <section id="welcome-section">
-          <h1>Hey there 👋🏼 I'm Tantely.</h1>
-          <p>Web Developer</p>
-        </section>
-        <section id="projects">
-          <h2>These are some of my projects</h2>
-          <div>
-            <div class="project-tile">
-              <a
-                href="https://github.com/tantely-rabefarihy/covid19-app-tracker"
-                target="_blank"
-              >
-                <img
-                  src="https://github.com/tantely-rabefarihy/covid19-app-tracker/raw/main/client/screenshots/Covid-app.png"
-                  alt="project"
-                />
-              </a>
-              <p class="description">Covid tracker app</p>
-            </div>
-            <div class="project-tile"></div>
-          </div>
-        </section>
-        <section id="collaboration">
-          <h2>Let's collaborate !</h2>
-
-          <a
-            id="profile-link"
-            href="https://github.com/tantely-rabefarihy"
-            target="_blank"
-          >
-            <p>You can see my Github profile</p>
-          </a>
-        </section>
-      </main>
+      <Main />
     </Layout>
   </React.Fragment>
 )
