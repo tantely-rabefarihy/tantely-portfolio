53dcf1b44f5eb01f37e852ac6e418ce6f70b38ab
Tantely Rabefarihy <tantelyrab@gmail.com>
2021-03-09 22:01:31 -0500

    update header

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,74 @@ import styled from "styled-components"
 import "../styles/styles.css"
 import logo from "../images/logo.png"
 
+const NavBar = styled.nav`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
+  background: transparent;
+
+  ul {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: fit-content;
+  }
+
+  li {
+    list-style-type: none;
+    padding: 0 1rem;
+  }
+
+  a {
+    text-decoration: none;
+    color: black;
+    font-size: 1rem;
+    font-weight: 400;
+    font-family: "Montserrat", sans-serif;
+    /* underline effect for the links */
+    text-transform: capitalize;
+    display: inline-block;
+    letter-spacing: 1px;
+    transition: all 0.3s linear;
+    position: relative;
+
+    :after {
+      background: none repeat scroll 0 0 transparent;
+      bottom: -10px;
+      content: "";
+      display: block;
+      height: 4px;
+      left: 50%;
+      position: absolute;
+      background: rgb(34, 32, 32);
+      transition: width 0.3s ease 0s, left 0.3s ease 0s;
+      width: 0;
+    }
+    :hover:after {
+      color: rgb(0, 0, 0);
+      width: 100%;
+      left: 0;
+    }
+  }
+`
+
+const Logo = styled.div`
+  padding: 1rem;
+
+  img {
+    width: 60px;
+    height: 40px;
+  }
+`
+
 const Header = () => {
   return (
     <>
-      <nav id="navbar">
-        <div className="logo">
+      <NavBar>
+        <Logo>
           <img src={logo} />
-        </div>
+        </Logo>
         <ul>
           <li>
             <a href="#welcome-section">About me</a>
@@ -21,7 +82,7 @@ const Header = () => {
             <a href="#collaboration">Contact</a>
           </li>
         </ul>
-      </nav>
+      </NavBar>
     </>
   )
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,16 +5,6 @@ const StyledLayout = styled.div`
   width: 100%;
   min-height: 100vh;
   margin: 0 auto;
-
-  /* display: grid;
-  grid-template-rows: auto 1fr auto;
-  grid-template-columns: 100%auto; */
-  /* #main-content {
-    width: 100%;
-    max-width: 62.5rem;
-    margin: 0 auto;
-    padding: 0 2.5rem;
-  } */
 `
 
 const Layout = ({ children }) => {
