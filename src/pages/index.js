54863e8402f69a339a8472ddb47b31152666ebc6
Tantely Rabefarihy <tantelyrab@gmail.com>
2021-04-06 11:03:11 -0400

    routing fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Layout from "../components/layout"
-import Helmet from "react-helmet"
 import NavBar from "../components/header/navbar"
 import GlobalStyle from "../styles/globalstyles"
 import Main from "../components/main"
