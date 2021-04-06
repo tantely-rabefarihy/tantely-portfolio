4a3f91882b6023f5b768f19776c3ebde31489a58
Tantely Rabefarihy <tantelyrab@gmail.com>
2021-04-05 23:19:50 -0400

    changing route name

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,19 +10,6 @@ import { ThemeProvider } from "styled-components"
 const HomePage = () => (
   <ThemeProvider theme={theme}>
     <>
-      {/* <Helmet>
-        <title>Tantely Rabefarihy - Full Stack Developer</title>
-        <meta
-          name="title"
-          content="Tantely Rabefarihy - Full Stack Developer"
-        />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          // href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;700&family=Poppins:wght@200;400;500;700&family=Questrial&display=swap"
-          href="https://fonts.googleapis.com/css2?family=Hind+Madurai:wght@300;400;500;600&family=Montserrat:wght@300;500;700&family=Questrial&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet> */}
       <GlobalStyle />
       <Layout>
         <NavBar />
