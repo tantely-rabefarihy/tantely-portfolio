17f5e05efbc1d917b149f91b9b7a5af87645b9f2
Tantely Rabefarihy <tantelyrab@gmail.com>
2021-04-06 15:58:37 -0400

    spacing

--- src/components/projectsPage.js
+++ src/components/projectsPage.js
@@ -127,9 +127,8 @@ export const GithubLink = styled.p`
 export const ImageContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-evenly;
-  row-gap: 3rem;
-  column-gap: 2rem;
+  justify-content: space-between;
+  gap: 3rem 2rem;
   padding: 1rem 0;
 
   @media only screen and (max-width: 568px) {
